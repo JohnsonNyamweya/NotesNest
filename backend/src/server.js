6893ac9e27a36ddb25d4667f7750f44f0f096a0f
JohnsonNyamweya <johnsonomari1032@gmail.com>
2025-08-15 08:28:01 +0300

    Prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,21 +4,34 @@ import dotenv from 'dotenv';
 import notesRoutes from './routes/notesRoutes.js';
 import { connectDB } from './config/db.js';
 import rateLimiter from './middleware/rateLimiter.js';
+import path, { dirname } from 'path';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
-app.use(cors(
-  {
-    origin: 'http://localhost:5173',
-  }
-));
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 app.use(express.json());
 app.use(rateLimiter);
 
 app.use('/api/notes', notesRoutes);
+
+// Serve static files from the React frontend app
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
